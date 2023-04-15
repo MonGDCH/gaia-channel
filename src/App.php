00ea28191050637d47665dbec317f76a6e98c903
zbc <15914718277@qq.com>
2023-04-15 09:55:51 +0800

    commit

---
+++ src/App.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace gaia\channel;
+
+/**
+ * 应用对象
+ * 
+ * @author Mon <985558837@qq.com>
+ * @version 1.0.0
+ */
+class App
+{
+    /**
+     * 程序版本号
+     * 
+     * @var string
+     */
+    const VERSION = '1.0.1';
+}
